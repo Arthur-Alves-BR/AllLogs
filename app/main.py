bfc9c164bf5eae1ef73047289f5d30c75bee9ab2
Arthur Alves <arthur.alves@radity.com>
2025-02-10 22:01:06 -0300

    feat: google's oauth2 integration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
-from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
+import os
 
-from .routers import healthcheck
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from tortoise.contrib.fastapi import register_tortoise
+from starlette.middleware.sessions import SessionMiddleware
+
+from .routers import healthcheck, auth
 
 app = FastAPI()
+
+app.include_router(auth.router)
 app.include_router(healthcheck.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY", "default_secret_key"))
+
 
 register_tortoise(
     app,
--- app/routers/healthcheck.py
+++ app/routers/healthcheck.py
@@ -1,8 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from app.core.jwt import get_current_user
+
 
 router = APIRouter()
 
 
 @router.get("/health_check")
-async def health_check() -> dict:
+async def health_check() -> dict[str, str]:
     return {"status": "Ok"}
+
+
+@router.get("/chat")
+async def get_response(current_user: Annotated[dict, Depends(get_current_user)]) -> dict[str, str]:
+    return {"message": "Welcome!", "user": current_user}
